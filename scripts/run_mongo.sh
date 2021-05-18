bf96fec7018ff483e8a4e6bf5dba3c0c11647979
SkymeFactor <sergei.51351@yandex.ru>
2021-05-18 11:57:12 +0300

    run mongo in docker (debug)

--- scripts/run_mongo.sh
+++ scripts/run_mongo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run --rm --network=host --name=mongo -d mongo >/dev/null
+docker run --rm -v $(pwd)/.database:/data/db --network=host --name=mongo -d mongo >/dev/null
 docker exec -it mongo bash
 docker stop mongo
